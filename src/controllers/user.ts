41ed358a7c6de46b7e6cd99fea73943f72e4e1e4
Nikhar S. <127284370+Nikhar-savaliya@users.noreply.github.com>
2024-04-30 18:53:18 +0530

    update Login api

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -71,39 +71,46 @@ const createUser = async (
  *
  */
 
-const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+const loginUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
   const { email, password } = req.body;
-  if (!email || !password) {
-    return next(createHttpError(400, "all fields are required"));
-  }
-  let user: User | null;
+
   try {
-    // finding user
-    user = await userModel.findOne({ email });
+    // Validation: Check if email and password are provided
+    if (!email || !password) {
+      throw createHttpError(400, "All fields are required");
+    }
+
+    // Find user by email
+    const user: User | null = await userModel.findOne({
+      "personal_info.email": email,
+    });
+
+    // Check if user exists
     if (!user) {
-      return next(createHttpError(404, "User not found"));
+      throw createHttpError(404, "User not found");
     }
-  } catch (error) {
-    return next(createHttpError(500, "error finding user from database"));
-  }
 
-  try {
-    // comparing password
-    const isMatch = verifyPassword(password, user.personal_info.password);
+    // Verify password
+    const isMatch: boolean = await verifyPassword(
+      password,
+      user.personal_info.password
+    );
     if (!isMatch) {
-      return next(createHttpError(400, "email or password incorrect"));
+      throw createHttpError(400, "Email or password incorrect");
     }
-  } catch (error) {
-    return next(createHttpError(500, "failed to compare password" + error));
-  }
-  try {
-    // token generation JWT
-    const token = generateJWtoken(user._id);
 
-    // response
-    res.status(201).json({ accessToken: token });
-  } catch (error) {
-    return next(createHttpError(500, "error while signing jwt token"));
+    // Generate JWT token
+    const token: string = generateJWtoken(user._id);
+
+    // Send response with JWT token
+    res.status(200).json({ accessToken: token });
+  } catch (error: any) {
+    // Handle errors
+    return next(createHttpError(500, "Error during login: " + error.message));
   }
 };
 
